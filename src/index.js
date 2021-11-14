ab2a566b7fd3cb5f85d2979116290ed5a89ba971
Simeon <yevlakhov@gmail.com>
2021-11-14 04:15:02 +0300

    Service Worker Registered

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
@@ -27,3 +28,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
